8d10549510db540d3d4faa8a96d01f2057089448
mituttta051 <amityutneva@mail.ru>
2025-05-28 09:10:54 +0300

    Extracted services uri in the application.yaml

--- src/main/java/cybercooker/apigateway/config/ScheduleServiceConfig.java
+++ src/main/java/cybercooker/apigateway/config/ScheduleServiceConfig.java
@@ -1,5 +1,6 @@
 package cybercooker.apigateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +9,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ScheduleServiceConfig {
 
+    @Value("${schedule.service.uri}")
+    private String scheduleServiceUri;
+
     @Bean
     public RouteLocator scheduleRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("schedule-service", p -> p
                         .path("/api/schedule/**")
                         .filters(f -> f.stripPrefix(2))
-                        .uri("http://localhost:8081"))
+                        .uri(scheduleServiceUri))
                 .build();
     }
 }
